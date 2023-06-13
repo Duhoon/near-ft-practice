bd6166044d972aa3c2f71931e0e32bb6133f72c0
Duhoon <eclip6@naver.com>
2023-06-13 10:30:01 +0900

    review: events

--- contract/src/events.rs
+++ contract/src/events.rs
@@ -6,23 +6,23 @@ use near_sdk::env;
 
 #[derive(Serialize, Debug)]
 #[serde(tag = "standard")]
-#[must_use = "don't forget to `.emit()` this event"]
+#[must_use = "dont't forget to `.emit()` this event"]
 #[serde(rename_all = "snake_case")]
-pub(crate) enum NearEvent<'a> {
+pub(crate) enum NearEvent<'a>{
     Nep141(Nep141Event<'a>),
 }
 
-impl<'a> NearEvent<'a> {
-    fn to_json_string(&self) -> String{ 
+impl<'a> NearEvent<'a>{
+    fn to_json_string(&self) -> String {
         #[allow(clippy::redundant_closure)]
         serde_json::to_string(self).ok().unwrap_or_else(|| env::abort())
     }
 
-    fn to_json_event_string(&self) -> String{
+    fn to_json_event_string(&self) -> String {
         format!("EVENT_JSON:{}", self.to_json_string())
     }
 
-    pub(crate) fn emit(self){
+    pub(crate) fn emit(self) {
         near_sdk::env::log_str(&self.to_json_event_string());
     }
 }
@@ -48,20 +48,20 @@ impl FtMint<'_>{
 
 #[must_use]
 #[derive(Serialize, Debug, Clone)]
-pub struct FtTransfer<'a>{
-    pub old_onwer_id: &'a AccountId,
+pub struct FtTransfer<'a> {
+    pub old_owner_id: &'a AccountId,
     pub new_owner_id: &'a AccountId,
     pub amount: &'a U128,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub memo: Option<&'a str>,
+    pub memo: Option<&'a str>
 }
 
 impl FtTransfer<'_> {
-    pub fn emit(self){
+    pub fn emit(self) {
         Self::emit_many(&[self])
     }
 
-    pub fn emit_many(data: &[FtTransfer<'_>]){
+    pub fn emit_many(data: &[FtTransfer<'_>]) {
         new_141_v1(Nep141EventKind::FtTransfer(data)).emit()
     }
 }
@@ -74,15 +74,15 @@ pub(crate) struct Nep141Event<'a>{
 }
 
 #[derive(Serialize, Debug)]
-#[serde(tag = "event", content= "data")]
+#[serde(tag = "event", content = "data")]
 #[serde(rename_all = "snake_case")]
 #[allow(clippy::enum_variant_names)]
-enum Nep141EventKind<'a> {
+enum Nep141EventKind<'a>{
     FtMint(&'a [FtMint<'a>]),
-    FtTransfer(&'a [FtTransfer<'a>]),
+    FtTransfer(&'a [FtTransfer<'a>])
 }
 
-fn new_141<'a>(version: &'static str, event_kind: Nep141EventKind<'a>) -> NearEvent<'a> {
+fn new_141<'a>(version: &'static str, event_kind: Nep141EventKind<'a>) -> NearEvent<'a>{
     NearEvent::Nep141(Nep141Event {version, event_kind})
 }
 
